99eb753c3006bb5c37a318764b144f8d7acce07d
988451835v <Kadnishedha@gmail.com>
2021-05-14 10:26:47 +0000

    Adding Answer for the first question

---
+++ Assignment1.scala
@@ -0,0 +1,9 @@
+object Assignment1 extends App{
+  def convert(x:Double)=x*1.8000+32.00;
+  var st="F";
+  println(convert(35)+st);
+
+/*def convert(x:Double)=x*1.8000+32.00;
+  println(convert(35)+" F"));*/
+
+}
